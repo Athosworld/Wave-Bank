cb403be5aecf27ca6ce7b3672974a397338a7591
Athos García <yuniaobird@gmail.com>
2025-11-21 15:50:10 -0800

    Update Class1.vb

--- WaveBankLib/WAVEBANKLIBVB/Class1.vb
+++ WaveBankLib/WAVEBANKLIBVB/Class1.vb
@@ -7,7 +7,7 @@ Namespace WBKPlayerLib
         Private Shared currentWBKBytes As Byte() = Nothing
         Private Shared currentWBKPath As String = ""
 
-        ' Load the WBK file into memory
+       
         Public Shared Sub LoadWBK(path As String)
             If Not File.Exists(path) Then
                 Throw New FileNotFoundException("WBK file not found", path)
@@ -16,19 +16,19 @@ Namespace WBKPlayerLib
             currentWBKBytes = File.ReadAllBytes(path)
         End Sub
 
-        ' Play a specific wave by its 3-digit ID
+        
         Public Shared Sub PlayWave(id As String)
             If currentWBKBytes Is Nothing Then
                 Throw New InvalidOperationException("WBK file not loaded.")
             End If
 
-            ' Extract WAV data
+           
             Dim wavPath As String = ExtractWavByID(id)
             If String.IsNullOrEmpty(wavPath) Then
                 Throw New FileNotFoundException("Wave ID not found: " & id)
             End If
 
-            ' Play the WAV
+            
             My.Computer.Audio.Play(wavPath, AudioPlayMode.Background)
         End Sub
 
@@ -43,7 +43,7 @@ Namespace WBKPlayerLib
                 If bank(i) = &H5 AndAlso bank(i + 1) = &H40 AndAlso bank(i + 3) = &H1 Then
                     Dim sa = bank(i + 4), sb = bank(i + 5), sc = bank(i + 6)
                     If sa = a AndAlso sb = b AndAlso sc = c Then
-                        ' find matching end tag
+                      
                         Dim j As Integer = i + 10
                         While j < bank.Length - 12
                             If bank(j) = &H5 AndAlso bank(j + 1) = &H40 AndAlso bank(j + 2) = &H95 AndAlso bank(j + 3) = &H1 Then
@@ -70,3 +70,4 @@ Namespace WBKPlayerLib
 
     End Class
 End Namespace
+
