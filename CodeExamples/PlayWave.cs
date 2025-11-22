56fdd2fbde88dd3e3df76473011894eff00b8d0f
Athos García <yuniaobird@gmail.com>
2025-11-21 16:18:56 -0800

    Update PlayWave.cs

--- CodeExamples/PlayWave.cs
+++ CodeExamples/PlayWave.cs
@@ -11,7 +11,7 @@ class Program
             var asm = Assembly.LoadFrom(AppDomain.CurrentDomain.BaseDirectory + "WAVEBANK.dll");
             var type = asm.GetType("WAVEBANK.WBKPlayerLib.Player");
             var player = Activator.CreateInstance(type);
-            type.GetMethod("LoadWBK").Invoke(player, new object[] { ".wbk" }); //Replace this with the path to the bank
+            type.GetMethod("LoadWBK").Invoke(player, new object[] { "BANK.wbk" }); //Replace this with the path to the bank
             type.GetMethod("PlayWave").Invoke(player, new object[] { "001" });
         }
         catch { }
